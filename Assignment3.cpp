5b65928cbb5edae88777a5872e4bfd21a5cfffc4
Sean Warnock <swarnock@warnocksolutions.com>
2018-06-29 18:54:33 -0700

    Initial push

---
+++ Assignment3.cpp
@@ -0,0 +1,26 @@
+/*
+Sean Warnock
+CSCI 14 Summer 2018
+Assignment 2 due 6/28/2018
+https://github.com/seanwarnock/CSCI14-Assignment2
+*/
+
+
+/*
+Start program
+output order menu
+tell user amount to get next discount.
+take input from user until user selects "done"
+output invoice.
+ask user if you want another order?
+*/
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#ifdef _WIN32
+  #include <windows.h>
+  #include <winsock2.h>
+#endif
+#ifdef __linux__
+
+#endif
