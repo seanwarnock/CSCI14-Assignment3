7704445c9c0555e91cbe2a7c444f4b7e6d8fd2fb
Sean Warnock <swarnock@warnocksolutions.com>
2018-06-30 23:28:21 -0700

    Base program working
    Base array creation working.

--- Assignment3.cpp
+++ Assignment3.cpp
@@ -1,19 +1,18 @@
 /*
 Sean Warnock
 CSCI 14 Summer 2018
-Assignment 2 due 6/28/2018
+Assignment 2 due 7/5/2018
 https://github.com/seanwarnock/CSCI14-Assignment2
 */
 
 
 /*
-Start program
-output order menu
-tell user amount to get next discount.
-take input from user until user selects "done"
-output invoice.
-ask user if you want another order?
+Start program with menu.
+1> Use functions for each menu item?
+2>
+
 */
+#include <time.h>
 #include <iostream>
 #include <iomanip>
 #include <math.h>
@@ -24,3 +23,73 @@ ask user if you want another order?
 #ifdef __linux__
 
 #endif
+
+using namespace std;
+
+ int main ()
+ {
+  bool boolRunAgain = true;
+
+  do
+  {
+    int ArraySize = 100;
+    int intCounter;
+    char charRunAgain;
+
+    srand((unsigned)time(NULL));
+
+
+
+
+
+
+
+
+
+
+
+
+    cout << "[1] Create Array and populate it with random numbers." << endl;
+    cout << "[2] Find highest and lowest value in the array." << endl;
+    cout << "[3] Sum of array and average" << endl;
+    cout << "[4] Provide SUM of EVEN and ODD array values" << endl;
+    cout << "[5] Find an integer value in the array and it's location" << endl;
+    cout << "[9] Exit" << endl;
+
+
+
+    // Array population stuff.
+
+    cout << "How large would you like your array? " << endl;
+    cin >> ArraySize;
+
+    float floatArrayTest[ArraySize] = {0};
+
+  //Time to stuff the array
+    for (intCounter=1; intCounter<=ArraySize; intCounter++)
+    {
+      floatArrayTest[intCounter] = rand()% ArraySize + 1;
+      cout << intCounter << "\t"  << floatArrayTest[intCounter] << endl;
+    }
+
+
+
+
+    boolRunAgain = true;
+    cout << "Would you like to continue this mess [Y] or n?" << endl;
+    cin >> charRunAgain;
+    if ((charRunAgain == 'N') or (charRunAgain == 'n'))
+      {
+        boolRunAgain = false;
+      }
+
+  }
+  while (boolRunAgain == true);
+  return 0;
+  /*
+  BTW this is standard output that is used to determine if a program ran successfully.
+  More of a *nix thing but handy as heck to use if you are controlling a program via
+  script to determine if the program ran normally or did something "off".
+  */
+ }
+
