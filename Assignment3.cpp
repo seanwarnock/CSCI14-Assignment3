5ca085d20729ce5a053fb330f949a4d154f3749f
Sean Warnock <swarnock@warnocksolutions.com>
2018-07-01 18:26:17 -0700

    Base program working
    Added the sum and average function.
    Skeleton section of high low calculation.

--- Assignment3.cpp
+++ Assignment3.cpp
@@ -27,23 +27,55 @@ Start program with menu.
 using namespace std;
 
 
-void arraymaker (int * NewArray, int intArrayToMake)
+int arraymaker (int * NewArray, int intMaxArraySize)
 {
+  int intArrayToMake;
   int intCounter;
 
+  srand((unsigned)time(NULL));
 
+  cout << "How large would you like your array?  Please choose a value from 1 to " << intMaxArraySize << endl;
+
+  cin >> intArrayToMake;
   //Time to stuff the array
-    for (intCounter=1; intCounter<=intArrayToMake; intCounter++)
+    for (intCounter=0; intCounter<=(intArrayToMake - 1); intCounter++)
     {
+
       NewArray[intCounter] = rand()% intArrayToMake + 1;
       cout << intCounter << "\t"  << NewArray[intCounter] << endl;
     }
+    system("pause");
+  return intArrayToMake;
 }
 
-void sumnation (int SumArray[])
+void HighLow (int HighLowArray[], int intArraySize)
 {
-  //
 
+
+
+}
+
+
+void sumnation (int SumArray[], int intArraySize)
+{
+/*
+This function takes in an array and a maximum value in the array to sum and average.
+This function assumes to start at the 0 position of the array.
+
+To-Do deal with the output of average to handle non-integer values.
+*/
+
+  int intCounter;
+  int intArraySum = 0;
+
+
+  for (intCounter=0; (intCounter <= intArraySize); intCounter++)
+  {
+    intArraySum = intArraySum + SumArray[intCounter];
+  }
+  cout << "The SUM of the array is: " << intArraySum << endl;
+  cout << "The average value of the array is:" << (intArraySum / intArraySize) << endl;
+  system("pause");
 
 }
 
@@ -52,17 +84,21 @@ void sumnation (int SumArray[])
  int main ()
  {
   bool boolRunAgain = true;
+  const int MaxArraySize = 10000;
+  int UserArraySize = 0;
+  int floatArrayTest[MaxArraySize];
 
   do
   {
-    int ArraySize = 100;
-    int floatArrayTest[ArraySize];
+
     char charRunAgain;
     float floatMenuChoice;
 
-    srand((unsigned)time(NULL));
 
 
+    //boolRunAgain = true;
+
+    system("cls");
     cout << "[1] Create Array and populate it with random numbers." << endl;
     cout << "[2] Find highest and lowest value in the array." << endl;
     cout << "[3] Sum of array and average" << endl;
@@ -72,62 +108,37 @@ void sumnation (int SumArray[])
 
     cin >> floatMenuChoice;
 
-  switch (int(floatMenuChoice))
-  {
-    case 1 :
-      cout << "How large would you like your array? " << endl;
-      cin >> ArraySize;
-      arraymaker(floatArrayTest,ArraySize);
-      break;
+    switch (int(floatMenuChoice))
+    {
+      case 1 :
+        UserArraySize = arraymaker(floatArrayTest, MaxArraySize);
+        break;
 
-    case 2 :
-      cout << "Highest and lowest value.";
-      break;
+      case 2 :
+        cout << "Highest and lowest value.";
+        HighLow(floatArrayTest, UserArraySize);
+        break;
 
-    case 3 :
-      cout << "sum time.";
-      break;
+      case 3 :
+        cout << "sum time.";
+        sumnation(floatArrayTest, UserArraySize);
+        break;
 
-    case 4 :
-      cout << "Even odd";
-      break;
+      case 4 :
+        cout << "Even odd";
+        break;
 
-    case 5 :
-      cout << "find stuff";
-      break;
+      case 5 :
+        cout << "find stuff";
+        break;
 
-    case 9 :
-      cout << "Exit!";
-      break;
-
-
-
-  }
-
-
-
-
-    // Array population stuff.
-
-
-
-
-
-
-    boolRunAgain = true;
-    cout << "Would you like to continue this mess [Y] or n?" << endl;
-    cin >> charRunAgain;
-    if ((charRunAgain == 'N') or (charRunAgain == 'n'))
-      {
+      case 9 :
+        cout << "Exit!";
         boolRunAgain = false;
-      }
-
+        break;
+  }
   }
   while (boolRunAgain == true);
+
   return 0;
-  /*
-  BTW this is standard output that is used to determine if a program ran successfully.
-  More of a *nix thing but handy as heck to use if you are controlling a program via
-  script to determine if the program ran normally or did something "off".
-  */
  }
